f6c92ae2e485f2fe5388d9e00f8af2d2b71c3975
borgearthur <143144251+borgearthur@users.noreply.github.com>
2024-04-08 12:25:15 -0300

    Update admin.py

--- cinema_app/cinema_app/admin.py
+++ cinema_app/cinema_app/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
-
-from .models import Horario
-from .models import Filme
+from .models import Filme, Alimento
 
 admin.site.register(Filme)
-admin.site.register(Horario)
+admin.site.register(Alimento)
