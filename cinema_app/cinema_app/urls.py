54667145025645abda1f7e8d42709a62d97094f7
borgearthur <tubodish@gmail.com>
2024-04-07 23:31:15 -0300

    Adapt for Azure (again)

--- cinema_app/cinema_app/urls.py
+++ cinema_app/cinema_app/urls.py
@@ -6,6 +6,6 @@ from . import views
 app_name = 'cinema_app'
 urlpatterns = [
     path('', views.home, name = 'home'),
-    path('cadastro/', views.alterar_filmes, name="cadastro"),
-    path('cadastrar_espaco/', views.criar_filmes, name='cadastrar_espaco'),   
+    path('alterar/', views.alterar_filmes, name="alterar"),
+    path('criar/', views.criar_filmes, name='criar'),   
 ]
--- cinema_app/cinema_app/views.py
+++ cinema_app/cinema_app/views.py
@@ -1,8 +1,28 @@
-
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
 from django.http import HttpResponse
+from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+
+def cadastro(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        name = request.POST['name']
+        email = request.POST['email']
+        password = request.POST['password']
+        
+        if User.objects.filter(username=username).exists():
+            return render(request, 'apps/cadastro.html', {"erro": "Usuário já existe"})
+        elif User.objects.filter(email=email).exists():
+            return render(request, 'apps/cadastro.html', {"erro": "Email já cadastrado"})
+
+        user = User.objects.create_user(username=username, password=password, email=email, first_name=name)
+        login(request, user)
+        request.session["usuario"] = username
+        return redirect(home)
+        
+    return render(request, 'apps/cadastro.html')
 
 def home(request):
     filmes = Filme.objects.all()
