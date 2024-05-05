83f18e15a2d90cab5a50b2b9460ea29fbcf4e0ce
borgearthur <tubodish@gmail.com>
2024-05-05 17:13:45 -0300

    ajuste em models e view

--- cinema_app/cinema_app/models.py
+++ cinema_app/cinema_app/models.py
@@ -1,15 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Genero(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
-    name = models.CharField(max_length=100)
-   
-    def __str__(self):
-        return self.name
 
-    class Meta:
-        unique_together = ('name', 'user')
 
 class Filme (models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
@@ -17,16 +9,21 @@ class Filme (models.Model):
     duracao = models.PositiveIntegerField(null=False, blank=False)
     data_de_lancamento = models.DateTimeField(null=False, blank=False)
     cartaz = models.FileField(upload_to='poster/')
-    genero = models.ForeignKey(Genero, on_delete=models.SET_NULL, null=True, blank=True)
-
+    nome = models.CharField(max_length=150, null=False, blank=False)
 
     def __str__(self):
         return self.nome
+    def get_short_description(self):
+        if len(self.descricao) > 100:
+            return self.descricao[:100].__add__("...")
+        else:
+            return self.descricao
+
 
 class Alimento (models.Model):
     nome = models.CharField(max_length=200, null=False)
-    preco = models.FloatField()
-    cartaz = models.FileField(upload_to='comida/')
+    preco = models.FloatField(null=False, blank=False)
+    foto_comida = models.FileField(upload_to='comida/', null=False, blank=False)
 
     def __str__(self):
         return self.nome
@@ -35,3 +32,11 @@ class Review(models.Model):
    user = models.ForeignKey(User, on_delete=models.CASCADE)
    filme = models.ForeignKey(Filme, on_delete=models.CASCADE)
    text = models.TextField(max_length=10000, default="minha Review legal!")
+
+class Favorito(models.Model):
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    filme = models.ForeignKey(Filme, on_delete=models.CASCADE)
+
+    def _str_(self):
+        return f'{self.usuario.username} - {self.filme.nome}'
+    
--- cinema_app/cinema_app/views.py
+++ cinema_app/cinema_app/views.py
@@ -1,125 +1,31 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect
-from django.contrib.auth.decorators import login_required
-from .models import *
-from .forms import *
 from django.views import View
-from .forms import *
-from django.views import View
-from django.http import HttpResponse
 from django.contrib.auth.models import User
-from django.contrib import messages
-from django.contrib.messages import constants
+from .models import *
 
-def home(req):
-    if(req.user.is_authenticated):
-        return render(req, 'cinema_app/home_user.html')
-        
-    return render(req, 'cinema_app/signin.html')
+def home(request):
+    filmes = Filme.objects.all()
+    alimentos = Alimento.objects.all()
+    return render(request, 'home.html', {'filmes': filmes}, {'alimentos': alimentos})
 
-class SignupView(View):
-    def get(self, req):
-        
-        return render(req, 'cinema_app/signup.html')
-
-    def post(self, req):
-        username = req.POST.get('username')
-        email = req.POST.get('email')
-        password = req.POST.get('password')
-        c_password = req.POST.get('c-password')
-
-        user = User.objects.filter(username=username)
-
-        if(user.exists()):
-            messages.add_message(req, constants.ERROR, 'Username já existe!')
-        else:
-            if(password == ""):
-                messages.add_message(req, constants.ERROR, 'Insira uma senha válida')
-                return render(req, 'cinema_app/signup.html')
-            
-            if(password != c_password):
-                messages.add_message(req, constants.ERROR, 'As senhas não são iguais!')
-            else:
-                user = User.objects.create_user(username=username, password=password, email=email)
-                user.save()
-
-                return redirect('/')
-
-        return render(req, 'cinema_app/signup.html')
-
-def filmes(req):
-    if(req.user.is_authenticated):
-        return redirect('/cinema_app')
-    
-    return render(req, 'cinema_app/filmes.html')
+def detalhes_filme(request, filme_id):
+    if request.user.is_authenticated:
+        filme = get_object_or_404(Filme, id=filme_id)
+        usuario = request.user
+        favorito = Favorito.objects.filter(usuario=usuario, filme=filme).exists()
+        detalhes_filme = filme.detalhes()
+        return render(request, 'detalhes.html', {'filme': filme, 'detalhes_filme': detalhes_filme, 'favorito':favorito})
+    else:
+        filme = get_object_or_404(Filme, id=filme_id)
+        detalhes_filme = filme.detalhes()
+        return render(request, 'detalhes.html', {'filme': filme, 'detalhes_filme': detalhes_filme})
 
 def getUser(req):
     user = User.objects.get(username=req.user)
     context = {"user": {"username": user.username,"email": user.email}}
     return context
 
-
-class ViewFilme(View):
-    def get(self, req, id):
-        if req.user.is_authenticated:
-            context = getUser(req)
-            try:
-                filme = Filme.objects.get(pk=id)
-                review = filme.review_set.filter(user=req.user)
-
-                allReviews = filme.review_set.all().exclude(user=req.user)
-
-                context["allReviews"] = allReviews
-
-                if(review.exists()):
-                    context["review"] = review.first()
-
-                
-                context["filme"] = filme
-                
-               
-                try:
-                    user_review = Review.objects.get(user=req.user, filme=filme)
-                    context["user_review"] = user_review.text
-                except Review.DoesNotExist:
-                    context["user_review"] = None
-                return render(req, 'cinema_app/filme.html', context)
-            except Exception as e:
-                print("An error occurred:", e)
-                return redirect("cinema_app:root")
-        
-        return render(req, "cinema_app/signin")
-
-    def post(self, req, id):
-        if req.POST.get("action") == "submit_review":
-            return self.criandoReview(req, id)
-        else:
-            return self.like_filme(req, id)
-
-    def criandoReview(self, req, id):
-        if req.user.is_authenticated:
-            try:
-                filme = Filme.objects.get(pk=id)
-                review_text = req.POST.get('textoDaReview')
-
-                existing_review = Review.objects.filter(user=req.user, filme=filme).first()
-
-                if existing_review:
-                    existing_review.text = review_text
-                    existing_review.save()
-                    return redirect(f"/cinema_app/review/{existing_review.id}")
-                
-                else:
-                    novaReview = Review(user=req.user, filme=filme, text=review_text)
-                    novaReview.save()
-
-                    return redirect(f"/cinema_app/review/{novaReview.id}")
-            except Filme.DoesNotExist:
-                return HttpResponse({"message": "Filme não encontrado"}, status=404)
-            
-        else:
-            return HttpResponse({"message": "Você precisa estar logado"}, status=400)
         
 class Search(View):
     def get(self, req):
@@ -154,27 +60,3 @@ class ReviewView(View):
                 return redirect("app:root")
             
         return redirect("app:root")
-
-@login_required
-def criar_filme(request):
-    if request.method == 'POST':
-        form = FormFilme(request.POST, user=request.user)
-        if form.is_valid():
-            filme = form.save(commit=False)
-            filme.user = request.user
-            filme.save()
-            return redirect('products')
-    else:
-        form = FormFilme(user=request.user)
-    return render(request, 'cinema_app/product_form.html', {'form': form})
-
-@login_required
-def create_category(request):
-    if request.method == 'POST':
-        form = FormGenero(request.POST)
-        if form.is_valid():
-            form.save(user=request.user)
-            return redirect('product-create')
-    else:
-        form = FormGenero()
-    return render(request, 'cinema_app/create_category.html', {'form':form})
