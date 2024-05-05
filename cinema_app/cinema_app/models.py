695274dbfc49c5f9145d76d2a8d55ec48cc08b72
borgearthur <tubodish@gmail.com>
2024-05-05 17:28:45 -0300

    correct model

--- cinema_app/cinema_app/models.py
+++ cinema_app/cinema_app/models.py
@@ -3,38 +3,40 @@ from django.contrib.auth.models import User
 
 
 
-    
-class Genero(models.Model):
-    name = models.CharField(max_length=100)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
-   
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        unique_together = ('name', 'user')
-
 class Filme (models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     nome = models.CharField(max_length=150, null=False, blank=False)
     duracao = models.PositiveIntegerField(null=False, blank=False)
     data_de_lancamento = models.DateTimeField(null=False, blank=False)
     cartaz = models.FileField(upload_to='poster/')
-    genero = models.ForeignKey(Genero, on_delete=models.SET_NULL, null=True, blank=True)
+    nome = models.CharField(max_length=150, null=False, blank=False)
 
+    def __str__(self):
+        return self.nome
+    def get_short_description(self):
+        if len(self.descricao) > 100:
+            return self.descricao[:100].__add__("...")
+        else:
+            return self.descricao
 
-    def __str__(self) -> str:
-        return self.name
 
 class Alimento (models.Model):
     nome = models.CharField(max_length=200, null=False)
-    preco = models.FloatField()
-    cartaz = models.FileField(upload_to='comida/')
+    preco = models.FloatField(null=False, blank=False)
+    foto_comida = models.FileField(upload_to='comida/', null=False, blank=False)
 
     def __str__(self):
-        return self.name
+        return self.nome
 
 class Review(models.Model):
    user = models.ForeignKey(User, on_delete=models.CASCADE)
    filme = models.ForeignKey(Filme, on_delete=models.CASCADE)
    text = models.TextField(max_length=10000, default="minha Review legal!")
+
+class Favorito(models.Model):
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    filme = models.ForeignKey(Filme, on_delete=models.CASCADE)
+
+    def _str_(self):
+        return f'{self.usuario.username} - {self.filme.nome}'
+    
