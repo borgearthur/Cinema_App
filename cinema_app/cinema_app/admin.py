2ffdc80566735c5b255c85013faef74851cdc2c7
borgearthur <tubodish@gmail.com>
2024-06-03 10:25:40 -0300

    Correct settings

--- cinema_app/cinema_app/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Filme, Alimento
-
-admin.site.register(Filme)
-admin.site.register(Alimento)
--- projeto/cinema/urls.py
+++ projeto/cinema/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for g3 project.
+URL configuration for Cinema_App project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
